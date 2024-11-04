cf654e801be0aaac2073fd9f02f71243d447638c
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-11-04 22:17:41 +0300

    add base of project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+import styles from "./App.module.css";
+
+export const App = () => {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>Output</div>
+      <div>Numbers</div>
+      <div>Operators</div>
+    </div>
+  );
+};
+
+// export default App
